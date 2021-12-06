dc0b0ccf55e4e64864437290e414531b97a73b00
noelle013 <elfisme146@gmail.com>
2021-12-06 21:49:47 +0800

    feat: add creat function

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
+const app = express()
+const port = 3000
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
-
-const app = express()
+const Restaurant = require('./models/restaurant')
 
 mongoose.connect('mongodb://localhost/restaurant-list')
-
 const db = mongoose.connection
 
 db.on('error', () => {
@@ -16,11 +16,6 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-const port = 3000
-
-const restaurantList = require('./restaurant.json')
-
-
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
@@ -28,9 +23,29 @@ app.set('view engine', 'handlebars')
 // setting static files
 app.use(express.static('public'))
 
+app.use(express.urlencoded({ extended: true }))
+
+// get 全部餐廳資料
 app.get('/', (req, res) => {
-  res.render('index', { restaurant: restaurantList.results})
+  Restaurant.find()
+    .lean()
+    .then(restaurant => res.render('index', { restaurant }))
+    .catch(error => console.log(error))
 })
+
+// Create
+app.get('/restaurant/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/restaurants', (req, res) => {
+  const newRestaurant = req.body
+  return Restaurant.create(newRestaurant)
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+
 
 app.get('/restaurants/:id', (req, res) => {
   const target = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.id)
