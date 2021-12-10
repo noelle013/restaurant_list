ab45d4bc47269c2aded50188aa336ef43de8b07b
noelle013 <elfisme146@gmail.com>
2021-12-10 14:41:38 +0800

    feat: rearrange

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ require('./config/mongoose')
 
 const app = express()
 
-
-
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
@@ -22,24 +20,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.use(routes)
-
-
-// Search
-app.get('/search', (req, res) => {
-  if (!req.query.keyword) {
-    res.redirect('/')
-  }
-  const keyword = req.query.keyword.trim().toLowerCase()
-  Restaurant.find({})
-    .lean()
-    .then(restaurants => {
-      const filterRestaurants = restaurants.filter(data => 
-        data.name.toLowerCase().includes(keyword) || data.category.includes(keyword)
-      )
-      res.render('index', { restaurant: filterRestaurants, keyword })
-    })
-    .catch(error => console.log(error))
-})   
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
